67bd80c4215e7a3662e5d14bc7ec5832ee8ba2db
AlexCruzeiro <alexcruzeiro@rocketmail.com>
2018-09-12 17:33:15 -0300

    FRONTEND - AMBIENTE DE DESENVOLVIMENTO

--- frontend/horacerta/src/app/layout-admin-lte/top-menu/top-menu.component.ts
+++ frontend/horacerta/src/app/layout-admin-lte/top-menu/top-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PessoaService } from '../../servicos/pessoa.service';
 
 @Component({
   selector: 'hr-top-menu',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopMenuComponent implements OnInit {
 
-  constructor() { }
+  pessoa: any;
+
+  constructor(private pessoaService: PessoaService) {
+    this.pessoaService.consultarPessoa().subscribe((pessoa) => {
+      this.pessoa = pessoa;
+    });
+   }
 
   ngOnInit() {
+   
   }
 
 
--- frontend/horacerta/src/app/servicos/pessoa.service.ts
+++ frontend/horacerta/src/app/servicos/pessoa.service.ts
@@ -1,3 +1,4 @@
+import { isDevMode } from '@angular/core';
 import { HR_API } from './../app.api';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie';
@@ -8,15 +9,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PessoaService {
   
-  public idPessoa: any = 1;
+  public idPessoa: any;
 
   constructor(private cookieService: CookieService, private http: HttpClient) { }
 
   ngOnInit() {
-    // this.idPessoa = this.cookieService.get('idPessoa');
   }
 
   consultarPessoa() {
+    if(!isDevMode()){
+      this.idPessoa = this.cookieService.get('idPessoa');
+    }
+    else {
+      this.idPessoa = 1;
+    }
     return this.http.get(`${HR_API}/pessoas/${this.idPessoa}`);
   }
 
