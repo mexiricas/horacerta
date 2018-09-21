60d71f3bdf3342f21cd3a9006485e92ce0457f7b
Gaspar <gaspar.monteiro@gmail.com>
2018-09-21 10:42:42 -0300

    FRONTEND - Correção de bug do saldo

--- frontend/horacerta/src/app/pipes/saldo.pipe.ts
+++ frontend/horacerta/src/app/pipes/saldo.pipe.ts
@@ -7,39 +7,43 @@ export class SaldoPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
 
-   var  obj = this.getHoursAndMinutes(value);
-   this.normalize(obj)   
-   return obj.horas+":"+obj.minutos;
+    var obj = this.getHoursAndMinutes(value);
+    this.normalize(obj)
+    return obj.horas + ":" + obj.minutos;
 
   }
 
-  getHoursAndMinutes(saldo:any){
+  getHoursAndMinutes(saldo: any) {
 
-    var saldoinminutes = Math.trunc(saldo/60000)
+    var saldoinminutes = Math.trunc(saldo / 60000)
     var horas = Math.trunc(saldoinminutes / 60)
     var minutos = saldoinminutes % 60
-    
-    return {horas:horas,minutos:minutos};
-    
+
+    return { horas: horas, minutos: minutos };
+
   }
 
-  isNegative(saldo){
+  isNegative(saldo) {
     return saldo < 0
   }
 
-  normalize(obj:any){
+  normalize(obj: any) {
 
-    if (obj.horas < 10 && obj.horas > 0 ){
-      obj.horas = "0"+obj.horas
-    } 
-    if(obj.horas > -9 && obj.horas < 0){
-      obj.horas = "-0"+Math.abs(obj.horas) 
+
+    if (obj.horas < 10 && obj.horas > 0) {
+      obj.horas = "0" + obj.horas
     }
-    if (obj.minutos < 0){
+    if (obj.horas > -9 ) {
+      obj.horas = "0" + Math.abs(obj.horas)
+    }
+    if (obj.minutos < 0 ) {
+      obj.horas = "-" + obj.horas
+    }
+    if (obj.minutos < 0) {
       obj.minutos = obj.minutos * -1
     }
-    if (obj.minutos < 10){
-      obj.minutos = "0"+obj.minutos
+    if (obj.minutos < 10) {
+      obj.minutos = "0" + obj.minutos
     }
 
   }
